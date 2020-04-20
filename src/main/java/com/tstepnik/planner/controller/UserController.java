4c8a1158a02331a7ac9a1aff1c4b6877ab501301
tstepnik <tomasz.stepnik77@gmail.com>
2020-04-20 17:00:49 +0200

    add DTO, MapStruck

--- src/main/java/com/tstepnik/planner/controller/UserController.java
+++ src/main/java/com/tstepnik/planner/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.tstepnik.planner.controller;
 import com.tstepnik.planner.domain.User.User;
+import com.tstepnik.planner.domain.User.UserDTO;
+import com.tstepnik.planner.domain.User.UserMapper;
 import com.tstepnik.planner.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,46 +14,46 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    private final UserMapper mapper;
 
     @GetMapping
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<List<User>> getUsers() {
-        return ResponseEntity.ok(userService.getUsers());
+    public ResponseEntity<List<UserDTO>> getUsers() {
+        List<User> users = userService.getUsers();
+
+        return ResponseEntity.ok(mapper.userToUserDTO(users));
     }
 
     @GetMapping("/me")
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<User> getLoggedUser(Principal principal) {
+    public ResponseEntity<UserDTO> getLoggedUser(Principal principal) {
         Optional<User> user = Optional.ofNullable(userService.getLoggedUser(principal));
         if (user.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return ResponseEntity.ok(user.get());
+        return ResponseEntity.ok(mapper.userToUserDTO(user.get()));
     }
 
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<User> getUser(@PathVariable("id") Long id){
-         return ResponseEntity.ok(userService.getUser(id));
-
+    public ResponseEntity<UserDTO> getUser(@PathVariable("id") Long id){
+        User user = userService.getUser(id);
+        return ResponseEntity.ok(mapper.userToUserDTO(user));
     }
 
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<User> updatedUser(@RequestBody User user, @PathVariable("id") Long id) {
+    public ResponseEntity<UserDTO> updatedUser(@RequestBody User user, @PathVariable("id") Long id) {
         Optional<User> updateUser = Optional.ofNullable(userService.updateUser(user, id));
         if (updateUser.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
-        return ResponseEntity.ok(updateUser.get());
+        return ResponseEntity.ok(mapper.userToUserDTO(updateUser.get()));
     }
 
     @DeleteMapping("/{id}")
